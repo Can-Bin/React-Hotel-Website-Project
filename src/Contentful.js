a1f2e200532d00a6f3e2139ba893ca0a160a60be
Can Bin <canbin60@gmail.com>
2022-11-24 21:39:27 +0300

    access token was encrypted

--- src/Contentful.js
+++ src/Contentful.js
@@ -2,10 +2,9 @@ import { createClient } from "contentful";
 
 const contentful = require("contentful");
 const client = contentful.createClient({
-  // This is the space ID. A space is like a project folder in Contentful terms
-  space: "qc65m5q492o5",
-  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: "s_pauvPzquEUP3NUeDf3dusmVSdjmXrVzthn26uTFKw"
+
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN
 });
 
 
